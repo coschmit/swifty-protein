fcc116e9e3abf9429fe0446f870aefeb304148b1
Colin <ccc>
2021-02-12 14:34:43 +0100

    Add base swift project

---
+++ swifty-protein/swifty_proteinApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_proteinApp.swift
+//  swifty-protein
+//
+//  Created by Colin Schmitt on 12/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct swifty_proteinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
